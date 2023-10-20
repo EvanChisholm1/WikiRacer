6d38f8070a9026b2a2a972331d2e8a59a1f9c86c
Evan Chisholm <elchisholm248@gmail.com>
2023-10-20 14:45:59 -0400

    got working speedrun program

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Wiki Racer is an AI system that aims to complete wikipedia speedruns at a super human level.
 
+Currently goes from Dune (novel) to Barbie (film) in just 5 moves.
---
+++ main.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+import numpy as np
+import requests
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2', device='cuda')
+
+def get_links(current_link):
+    html = requests.get(current_link)
+    soup = BeautifulSoup(html.content, features='html.parser')
+    links = soup.find_all('a')
+
+    possible_pages = []
+
+    for link in links:
+        title = link.get('title')
+        href = link.get('href')
+
+        if title is None:
+            continue
+
+        possible_pages.append((title, href))
+
+    return possible_pages
+
+def calculate_similarity(a, b):
+    dot_product = np.dot(a, b)
+    normA = np.linalg.norm(a)
+    normB = np.linalg.norm(b)
+    sim = dot_product / (normA * normB);
+    return sim
+
+def get_next_page(current_link, target_href, target_embedding, visited):
+    possible_pages = get_links(current_link)
+
+    most_similar = float('-inf')
+    best_link = ""
+    best_title = ""
+
+    for title, link in possible_pages:
+        if link == target_href:
+            return title, link
+
+        if title in visited:
+            continue
+
+        sim = calculate_similarity(target_embedding, model.encode(title))
+        if sim > most_similar:
+            most_similar = sim
+            best_link = link
+            best_title = title
+    
+    return best_title, best_link
+
+def speedrun(start_url, end_url):
+    start_html = requests.get(start_url).content
+    end_html = requests.get(end_url).content
+    
+    start_soup = BeautifulSoup(start_html, features='html.parser')
+    end_soup = BeautifulSoup(end_html, features='html.parser')
+
+    start_title = start_soup.find('h1').get_text()
+    end_title = end_soup.find('h1').get_text()
+
+    print("starting at:", start_title)
+    print("trying to find:", end_title)
+
+    target_embedding = model.encode(end_title)
+
+    visited = []
+    title, link = get_next_page(start_url, end_url, target_embedding, visited)
+    visited.append(title)
+    
+    distance = 1
+    
+    while True:
+        print("At:", title)
+
+        if title == end_title:
+            print("finished in", distance, "moves")
+            break
+
+        title, link = get_next_page(f'https://en.wikipedia.org/{link}', end_url, target_embedding, visited)
+        visited.append(title)
+        distance += 1
+
+
+# speedrun("https://en.wikipedia.org/wiki/Dune_(novel)", "https://en.wikipedia.org/wiki/Barbie_(film)")
+
+start = input('input starting url: ')
+end = input('input ending url: ')
+
+speedrun(start, end)
